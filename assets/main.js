596290afe6d1d7d53b446a8f6a82b2d64457730f
thanneman <thanneman1@gmail.com>
2020-06-04 17:59:56 -0700

    added side nav/updated header

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,27 @@
 $(document).ready(function() {
-  // Auto set footer copyright date
+  // auto set footer copyright date
   var date = new Date();
   var year = date.getFullYear();
   $('.year').text(year);
+
+  // open side nav
+  $('.openbtn').click(function() {
+    $('#mySidenav').css('width', '250px');
+  });
+
+  // close side nav
+  $('.closebtn').click(function() {
+    $('#mySidenav').css('width', '0px');
+  });
+
+  // auto scroll
+  $('.nav-link').click(function(e) {
+    var jump = $(this).attr('href');
+    var new_position = $(jump).offset();
+    $('#mySidenav').css('width', '0px');
+    $('html, body')
+      .stop()
+      .animate({ scrollTop: new_position.top }, 500);
+    e.preventDefault();
+  });
 });
