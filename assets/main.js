31715d68f370120af3843786d9438e1e2a963ffa
thanneman <thanneman1@gmail.com>
2020-06-10 23:19:12 -0700

    add project images

--- assets/main.js
+++ assets/main.js
@@ -1,9 +1,4 @@
 $(document).ready(function() {
-  // auto set footer copyright date
-  var date = new Date();
-  var year = date.getFullYear();
-  $('.year').text(year);
-
   // open side nav
   $('.openbtn').click(function() {
     $('#mySidenav').css('width', '160px');
@@ -15,13 +10,32 @@ $(document).ready(function() {
   });
 
   // auto scroll
-  $('.nav-link').click(function(e) {
+  $('.nav-link').click(function() {
     var jump = $(this).attr('href');
     var new_position = $(jump).offset();
+    $('.nav-link').removeClass('active');
+    $(this).addClass('active');
     $('#mySidenav').css('width', '0px');
     $('html, body')
       .stop()
       .animate({ scrollTop: new_position.top }, 500);
     e.preventDefault();
   });
+
+  // // fade in elements
+  // $(window).scroll(function() {
+  //   // fade in projects, work, skills
+  //   $('.project-card, .work-sub, .skills-sub').each(function() {
+  //     var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+  //     var bottom_of_window = $(window).scrollTop() + $(window).height();
+  //     if (bottom_of_window > bottom_of_object) {
+  //       $(this).animate({ opacity: '1' }, 1000);
+  //     }
+  //   });
+  // });
+
+  // auto set footer copyright date
+  var date = new Date();
+  var year = date.getFullYear();
+  $('.year').text(year);
 });
