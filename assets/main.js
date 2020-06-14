01e06e37027d8ea3f8ce10f29a88eaf1bb6cb264
thanneman <thanneman1@gmail.com>
2020-06-14 13:18:15 -0700

    fixed header sizing bug

--- assets/main.js
+++ assets/main.js
@@ -22,18 +22,6 @@ $(document).ready(function() {
     e.preventDefault();
   });
 
-  // // fade in elements
-  // $(window).scroll(function() {
-  //   // fade in projects, work, skills
-  //   $('.project-card, .work-sub, .skills-sub').each(function() {
-  //     var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-  //     var bottom_of_window = $(window).scrollTop() + $(window).height();
-  //     if (bottom_of_window > bottom_of_object) {
-  //       $(this).animate({ opacity: '1' }, 1000);
-  //     }
-  //   });
-  // });
-
   // auto set footer copyright date
   var date = new Date();
   var year = date.getFullYear();
